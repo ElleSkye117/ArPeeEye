322be74f75b443444e361d152d1c5f7e4d0ca146
Ryan <github@ryan.codes>
2018-08-26 07:42:11 -0600

    Update .gitignore, add basic project

---
+++ ArePeeEye/ArePeeEye.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for ArePeeEye.
+
+FOUNDATION_EXPORT double ArePeeEyeVersionNumber;
+
+//! Project version string for ArePeeEye.
+
+FOUNDATION_EXPORT const unsigned char ArePeeEyeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArePeeEye/PublicHeader.h>
